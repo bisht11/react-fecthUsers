e3a47ca64c7adb479cfb620bf71c4f949f667c94
Himanshu Bisht <hbisht899@gmail.com>
2023-03-22 23:00:39 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import loader from "./assets/loader.gif";
+
+// user data API
+const URL = "https://reqres.in/api/users?page=1";
 
 function App() {
+  const [users, setUsers] = useState([]); // state to manage user data
+  const [loading, setLoading] = useState(false);
+
+  // funtion to fetch user data
+  const fetchUsers = async () => {
+    setLoading(true); // show loading screen when data is loading
+    const response = await fetch(URL);
+    const data = await response.json();
+    setLoading(false); // stop showing loading message once data is loaded
+    setUsers(data.data); // update user data state
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <h1>BRAND</h1>
+        <button className="userBtn" onClick={fetchUsers}>
+          Get Users
+        </button>
+      </nav>
+      {loading ? (
+        <div className="loader">
+          <img src={loader} alt="loader" />
+        </div>
+      ) : (
+        <div className="card-grid">
+          {/* map through users array */}
+          {users.map((user) => (
+            <div key={user.id} className="card">
+              <img src={user.avatar} alt={user.first_name} />
+              <h2>
+                {/* display user details */}
+                {user.first_name} {user.last_name}
+              </h2>
+              <p>{user.email}</p>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
